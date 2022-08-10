502181dbf926b5f6f00af8e55544ce9b1923210f
Nikita Proshyn <nikitaproshyn@gmail.com>
2022-08-11 00:14:55 +0300

    working with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
 // @ts-ignore
-import styles from './app.module.css';
+import styles from './App.module.css';
+
+const Button = ({ title, click, className, condition }: any) => {
+   return (
+      <button
+         onClick={click}
+         className={`${styles.button} ${className || ''}`}
+         disabled={condition}
+      >
+         {title}
+      </button>
+   );
+};
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className={styles.app}>
+         <Button
+            title={'Primary'}
+            click={() => alert('Hello from Primary')}
+            className={styles.primary}
+         />
+
+         <Button title={'Primary'} condition={true} />
+
+         <Button
+            title={'Secondary'}
+            click={() => alert('Hello from Secondary')}
+            className={styles.secondary}
+         />
+
+         <Button title={'Secondary'} condition={true} />
+
+         <Button
+            title={'Secondary2'}
+            click={() => alert('Hello from Secondary2')}
+            className={styles.secondary2}
+         />
+
+         <Button
+            title={'Secondary2'}
+            condition={true}
+            className={styles.secondary2Disabled}
+         />
+      </div>
+   );
 }
 
 export default App;
