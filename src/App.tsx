cb5034ae2749583639690259c30ea1682280afba
Nikita Proshyn <nikitaproshyn@gmail.com>
2022-08-14 18:39:56 +0300

    added two components

--- src/App.tsx
+++ src/App.tsx
@@ -15,47 +15,65 @@ const Button = ({ title, click, className, condition }: any) => {
 };
 
 const User = ({ userName }: any) => {
-   return (
-      <div className={styles.user}>
-         <p>{userName}</p>
-      </div>
-   );
+   return <p>{userName}</p>;
+};
+
+const Title = ({ title, className }: any) => {
+   return <h2 className={className}>{title}</h2>;
+};
+
+const Tabs = ({ title, className }: any) => {
+   return <p className={`${styles.tab} ${className || ''}`}>{title}</p>;
 };
 
 function App() {
    return (
       <div className={styles.app}>
          <div className={styles.buttons_wrap}>
-            <Button
-               title={'Primary'}
-               click={() => alert('Hello from Primary')}
-               className={styles.primary}
-            />
+            <div className={styles.primary_wrap}>
+               <Button
+                  title={'Primary'}
+                  click={() => alert('Hello from Primary')}
+                  className={styles.primary}
+               />
 
-            <Button title={'Primary'} condition={true} />
+               <Button title={'Primary'} condition={true} />
+            </div>
+            <div className={styles.secondary_wrap}>
+               <Button
+                  title={'Secondary'}
+                  click={() => alert('Hello from Secondary')}
+                  className={styles.secondary}
+               />
 
-            <Button
-               title={'Secondary'}
-               click={() => alert('Hello from Secondary')}
-               className={styles.secondary}
-            />
+               <Button title={'Secondary'} condition={true} />
+            </div>
+            <div className={styles.secondary2_wrap}>
+               <Button
+                  title={'Secondary2'}
+                  click={() => alert('Hello from Secondary2')}
+                  className={styles.secondary2}
+               />
 
-            <Button title={'Secondary'} condition={true} />
-
-            <Button
-               title={'Secondary2'}
-               click={() => alert('Hello from Secondary2')}
-               className={styles.secondary2}
-            />
-
-            <Button
-               title={'Secondary2'}
-               condition={true}
-               className={styles.secondary2Disabled}
-            />
+               <Button
+                  title={'Secondary2'}
+                  condition={true}
+                  className={styles.secondary2Disabled}
+               />
+            </div>
+         </div>
+         <div className={styles.user}>
+            <User userName={'Artem Malkin'} />
+         </div>
+         <div className={styles.title_wrap}>
+            <Title title={'Sign In'} className={styles.title} />
          </div>
 
-         <User userName={'Artem Malkin'} />
+         <div className={styles.tabs_wrap}>
+            <Tabs title={'All'} className={styles.all} />
+            <Tabs title={'My Favorites'} className={styles.favorite} />
+            <Tabs title={'Popular'} className={styles.popular} />
+         </div>
       </div>
    );
 }
